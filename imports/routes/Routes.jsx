4c25db58f8eb54d55270451f42ca4c6c8c49afc1
manhhailua <manhhailua@gmail.com>
2017-01-05 01:40:13 +0700

    fix IndexRoute

--- imports/routes/Routes.jsx
+++ imports/routes/Routes.jsx
@@ -5,7 +5,7 @@
 /* eslint-disable import/prefer-default-export */
 
 import React, { Component } from 'react';
-import { Router, Route, Redirect, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from '../ui/components/App.jsx';
 import Home from '../ui/pages/Home.jsx';
@@ -18,12 +18,12 @@ class Routes extends Component {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={App}>
+          <IndexRoute component={Home} />
           <Route path="home" component={Home} />
           <Route path="shopping" component={Shopping} />
           <Route path="order" component={Order} />
           <Route path="*" component={Page404} />
         </Route>
-        <Redirect from="/" to="/home" />
       </Router>
     );
   }
