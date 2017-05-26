30635d8e2852fbc80ad9e708ffcf947235d165d5
Manh <manhpt2@fsoft.com.vn>
2017-05-27 00:47:48 +0900

    try sass features

--- client/main.js
+++ client/main.js
@@ -5,8 +5,8 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
 import App from '../imports/ui/components/App';
+import './main.scss';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
